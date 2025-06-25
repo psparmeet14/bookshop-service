0bb489a595e0a493bbe83e705b03b8badc3de4ed
Parmeet Singh <parmeet.singh@catalystone.com>
2025-06-25 09:01:50 +0530

    Initialize bookshop project

---
+++ src/main/java/com/psb/bookshop/BookshopServiceApplication.java
@@ -0,0 +1,13 @@
+package com.psb.bookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshopServiceApplication.class, args);
+	}
+
+}
